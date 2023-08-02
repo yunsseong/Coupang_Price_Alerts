eacf2c50f67ced98a4094cc4827085a44edeb522
yunsseong <aster20@hanyang.ac.kr>
2023-08-02 15:39:34 +0900

    함수 기능 버그 수정, 새로운 기능 함수 작성
    함수 기능 버그 수정
    크롤링 시 가장 첫 번째 요소가 광고인 경우가 있어서 광고 여부에 따른 분기 처리 도입
    
    새로운 기능 함수 작성
    gen_link(product_name)
    - 제품 이름을 매개변수로 받아 해당 쿠팡 제품의 주소를 문자열로 반환하는 함수
    
    gen_product_price(product_name)
    - 제품 이름을 매개변수로 받아 해당 상품의 가격 정보를 딕셔너리로 반환하는 함수

--- coupang_crawler/crawler_main.py
+++ coupang_crawler/crawler_main.py
@@ -15,7 +15,7 @@ options.add_argument("user-agent=" + "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_
 options.add_argument("sec-ch-ua-platform=" + "macOS")
 options.add_argument("cookie=" + "PCID=31489593180081104183684; _fbp=fb.1.1644931520418.1544640325; gd1=Y; X-CP-PT-locale=ko_KR; MARKETID=31489593180081104183684; sid=03ae1c0ed61946c19e760cf1a3d9317d808aca8b; x-coupang-origin-region=KOREA; x-coupang-target-market=KR; x-coupang-accept-language=ko_KR;")
 
-# 제품 이름을 입력하면 가장 첫번째로 뜨는 상품의 제목과 가격, 링크를 반환하는 함수
+# 제품 이름을 매개변수로 받아 가장 첫 번째로 뜨는 상품의 제목과 가격, 링크를 리스트로 반환하는 함수
 def find_product_by_name(search_query):
     driver = webdriver.Chrome(options=options)
     driver.get(f"https://www.coupang.com/np/search?q={search_query}")
@@ -25,7 +25,7 @@ def find_product_by_name(search_query):
 
     return [product_name.text, product_price.text, product_link.text]
 
-# 제품 이름을 입력하면 가장 첫번쨰로 뜨는 상품의 itemId를 반환하는 함수
+# 제품 이름을 매개변수로 받아 가장 첫 번쨰로 뜨는 제품의 product_number를 반환하는 함수
 def get_item_id(search_query):
     driver = webdriver.Chrome(options=options)
     driver.get(f"https://www.coupang.com/np/search?q={search_query}")
@@ -33,8 +33,39 @@ def get_item_id(search_query):
     item_id = parse_qs(urlparse(product_link).query)['itemId'][0]
     return item_id
 
-print(get_item_id("맥북에어"))
+# 제품 이름을 매개변수로 받아 광고를 제외한 첫 번쨰로 뜨는 제품의 product number를 반환하는 함수
+def get_product_num(product_name):
+    driver = webdriver.Chrome(options=options)
+    driver.get(f"https://www.coupang.com/np/search?q={product_name}")
+    products = driver.find_elements(By.CLASS_NAME, "search-product-link")
 
+    try:
+        ad_check = products[0].find_element(By.CLASS_NAME, "ad-badge ad-position-D")
+        product = products[0]
+    except:
+        product = products[1]
+
+    product_num = product.get_attribute('href').split('/')[-1].split('?')[0]
+    return product_num
+
+# 제품 이름을 매개변수로 받아 해당 쿠팡 제품의 주소를 문자열로 반환하는 함수
+def gen_link(product_num):
+    return f"https://www.coupang.com/vp/products/{product_num}"
+
+# 제품 이름을 매개변수로 받아 해당 상품의 가격 정보를 딕셔너리로 반환하는 함수
+def get_product_price(product_name):
+    url = gen_link(get_product_num(product_name))
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+    product_price_div = driver.find_element(By.CLASS_NAME, "prod-price")
+    origin_price = product_price_div.find_element(By.CLASS_NAME, "origin-price").text
+    discount_rate = product_price_div.find_element(By.CLASS_NAME, "discount-rate").text
+    total_price = product_price_div.find_element(By.CLASS_NAME, "total-price").text
+
+    product_price = {"origin_price" : origin_price, "discount_rate" : discount_rate, "total_price" : total_price}
+    return product_price
+
+print(get_product_price("맥북에어 13 M1"))
 
 
 
